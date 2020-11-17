df7a1442fca53b79d06d28ca5b1b79e4978f3486
Slackoth <00082318@uca.edu.sv>
2020-11-17 12:04:03 -0600

    [ADDED] Limit of times the user can try to login.

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -11,8 +11,15 @@ use app\core\Application;
 
 
 
-class LoginForm  extends  FormModel
-{
+class LoginForm  extends  FormModel {
+    private $tries;
+
+    public function __construct() {
+        if(!isset($_SESSION["login_tries"]))
+            Application::getInstance()->session->setAttribute("login_tries", 0);
+        else
+            $this->tries = Application::getInstance()->session->getAttribute("login_tries");
+    }
 
     public function tablename() {
         return "user";
@@ -28,9 +35,9 @@ class LoginForm  extends  FormModel
     public function rules()
     {
         return [
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
-            'password' => [self::RULE_REQUIRED, self::RULE_INVALID_LOGIN,
-                ],
+            // 'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            // 'password' => [self::RULE_REQUIRED, self::RULE_INVALID_LOGIN,
+            //     ],
         ];
     }
 
@@ -42,33 +49,49 @@ class LoginForm  extends  FormModel
         ];
     }
 
-    public function login()
-    {
+    public function login() {
+        $now = time();
+
+        if($this->tries >= 3) {
+            Application::getInstance()->session->setAttribute("waiting_time", $now + 300);
+            Application::getInstance()->session->setAttribute("login_tries", 0);
+            
+            return false;
+        }
+        else if(isset($_SESSION["waiting_time"]) && $_SESSION["waiting_time"] > $now) {
+            $_SESSION['errors'] = 
+                array("Ha superado el límite de intentos para ingresar a su cuenta. Espere 5 minutos.");
+            
+            Application::getInstance()->response->redirect("login");
+            exit;
+        }
 
         $user = Application::getInstance()->db->findOne( 'user',['email' => $this->email], ['password']);
-        //echo var_dump('user',$user['password']);
-        //echo  var_dump('data',$this->password);
-        //echo var_dump(substr($user['password'],0,strlen($user['password'])-1));
+        
         $pswd_substr=substr($user['password'],0,strlen($user['password'])-1);
+
         if (empty($this->password) or empty($this->email)){
             $_SESSION['errors'] = array("Por favor complete los campos correctamente");
             return false;
         }
         if (!$user) {
-            $_SESSION['errors'] = array("Este Usuario es Inexistente");
+            $this->tries++;
+            Application::getInstance()->session->setAttribute("login_tries", $this->tries);
+            $_SESSION['errors'] = array("Usuario / Contraseña Incorrecta");
+
             return false;
         }
         if (!password_verify($this->password, $pswd_substr)) {
-            //$this->addError('password', 'Password is incorrect');
-            $message="Usuario / Contraseña equivocado";
-            //echo "<script>alert('$message');</script>";
+            $this->tries++;
+            Application::getInstance()->session->setAttribute("login_tries", $this->tries);
             $_SESSION['errors'] = array("Usuario / Contraseña Incorrecta");
+            
             return false;
         }
-        $userinfo=Application::getInstance()->db->findOne('user',['email' => $this->email], ['id', 'firstname', 'lastname', "is_admin"]);
-        //var_dump("userinfo: : : ", $userinfo);
+
+        $userinfo = Application::getInstance()->db->findOne('user',['email' => $this->email], ['id', 'firstname', 'lastname', "is_admin"]);
+        
         Application::getInstance()->session->setUser($userinfo);
-                //return Application::->login($user);
 
         return true;
     }
